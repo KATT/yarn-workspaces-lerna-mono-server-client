8a7e5db273fe96a38909db8ba3913d69ac896e11
KATT <alexander@n1s.se>
2020-11-10 16:51:31 +0100

    lerna linked

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {sum} from "@katt/shared"
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <p className={styles.description}>Two plus Two is {sum(2,2)} - quick maths.</p>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
